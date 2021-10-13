e8450592956cadaf5a0cbc0da8f473d3a6ef084d
sim2kid <sim2kid@users.noreply.github.com>
2021-10-12 23:28:38 -0500

    Fisher Yates Done

--- Fisher-Yates Shuffle/FisherYates.cs
+++ Fisher-Yates Shuffle/FisherYates.cs
@@ -4,22 +4,62 @@ using System.Text;
 
 namespace Fisher_Yates_Shuffle
 {
-    class FisherYates
+    public static class FisherYates
     {
-        string fileToRead = "data.csv";
-        
+        // This is our random number generator (Psudo-Random)
+        private static Random randomNumber = new Random();
 
-
-        private static List<string> LoadCVS(string filelocation) 
+        public static object[] FisherYatesShuffle(this object[] objects) 
         {
-            List<string> output = new List<string>();
-            string text = System.IO.File.ReadAllText(filelocation);
-            string[] words = text.Split(",");
-            foreach (string word in words) 
+            // We loop through the array starting from the end and going down
+            for (int i = objects.Length - 1; i > 0; i--) 
             {
-                output.Add(word);
+                // We swap out current object with one at random betweem the begining of the array and the current object (so it might not get swapped)
+                Swap(objects, i, GetRandomNumber(i + 1));
+                // We only swap a number once so once it's randomized, it isn't touched again.
+                // That gives this specific Algorithm an O(N) at Max and Min cases.
             }
-            return output;
+            return objects;
+        }
+
+        public static object[] AltFisherYatesShuffle(this object[] objects)
+        {
+            // This is the same algorithm just going from the start to the end of the array
+            for (int i = 0; i < objects.Length-2; i++)
+            {
+                // The specifications set j between 0 ≤ j < n-i and have the 2nd position a[i+j]
+                // If you look at the maths, that mean if the 2nd pos is a[j], then j = rand(max - i) + i
+                // and that is the equivlent to the Min, Max random math, so i just added that
+                Swap(objects, i, GetRandomNumber(i, objects.Length));
+            }
+            return objects;
+        }
+
+        private static void Swap(this object[] objects, int i, int j) 
+        {
+            // This just swaps two object's places in an array
+            object temp = objects[i];
+            objects[i] = objects[j];
+            objects[j] = temp;
+        }
+
+        /// <summary>
+        /// Return a random number between [<paramref name="min"/>, <paramref name="max"/>)
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        private static int GetRandomNumber(int min, int max) 
+        {
+            // This returns the next random number between [min,max) where max is exclusive and min is inclusive.
+            
+            return randomNumber.Next(max - min) + min;
+        }
+        private static int GetRandomNumber(int max)
+        {
+            // an overflow where min = 0
+            // Also the guy in the video added 1 to the max like a dumb dumb... so I didn't do that.
+            return GetRandomNumber(0, max);
         }
     }
 }
--- Fisher-Yates Shuffle/Program.cs
+++ Fisher-Yates Shuffle/Program.cs
@@ -7,7 +7,32 @@ namespace Fisher_Yates_Shuffle
     {
         static void Main(string[] args)
         {
-            
+            List<string> Words = LoadCVS("data.csv");
+            Console.WriteLine($"Before Shuffle:\n{WriteList(Words)}");
+            Words = ArrayToList<string>(FisherYates.AltFisherYatesShuffle(Words.ToArray()));
+            Console.WriteLine($"\nAfter Shuffle:\n{WriteList(Words)}");
+        }
+
+        private static List<T> ArrayToList<T>(object[] array) 
+        {
+            List<T> list = new List<T>();
+            foreach (object o in array)
+                list.Add((T)o);
+            return list;
+        }
+
+        private static string WriteList(List<string> strs) 
+        {
+            string back = string.Empty;
+            foreach (string s in strs)
+                back += $"{s} ";
+            return back;
+        }
+
+        private static List<string> LoadCVS(string filelocation)
+        {
+            string text = System.IO.File.ReadAllText(filelocation);
+            return ArrayToList<string>(text.Split(","));
         }
     }
 }
